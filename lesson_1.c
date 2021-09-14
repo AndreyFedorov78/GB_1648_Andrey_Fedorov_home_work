beb048bd38fe554907c16ee262be052a8dd60775
Andrey Fedorov <7644048@bk.ru>
2021-09-15 00:02:11 +0300

    'lesson_1.c_added'

--- lesson_1.c
+++ lesson_1.c
@@ -2,7 +2,7 @@
 
 int main(void){
   printf("task 1:\n");
-	printf("My\n   name\n      is\n         Andrey\n\n");
+	printf("My\n   name\n      is\n        Andrey\n\n");
 	printf("task 2:\n");
 	printf("   *\n  ***\n *****\n HHHHH\n ZZZZZ\n");
 	return(0);
